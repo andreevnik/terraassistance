960c3c1f119ffb722dd6bee249877e728b7a2b2a
Nikolay Andreev <andreev_nikolay@outlook.com>
2025-05-29 17:53:38 +0400

    Add IProblemRepository and IRepository interfaces for problem management

---
+++ TerraAssistance.ProblemManagement.Domain/Interfaces/IProblemRepository.cs
@@ -0,0 +1,7 @@
+namespace TerraAssistance.ProblemManagement.Domain.Interfaces;
+
+using TerraAssistance.ProblemManagement.Domain.Entities;
+
+public interface IProblemRepository : IRepository<Problem>
+{
+}
---
+++ TerraAssistance.ProblemManagement.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+namespace TerraAssistance.ProblemManagement.Domain.Interfaces;
+
+using TerraAssistance.ProblemManagement.Domain.Entities.Abstract;
+
+public interface IRepository<TId, TEntity>
+    where TId : struct, IEquatable<TId>
+    where TEntity : Entity<TId>
+{
+    Task<TEntity?> TryFindByIdAsync(TId id, CancellationToken cancellationToken);
+
+    Task<TEntity> FindByIdAsync(TId id, CancellationToken cancellationToken);
+
+    Task<IReadOnlyCollection<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+
+    void Add(TEntity entity);
+
+    void Update(TEntity entity);
+
+    void Remove(TEntity entity);
+
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+}
+
+public interface IRepository<TEntity> : IRepository<int, TEntity>
+    where TEntity : Entity
+{
+}
