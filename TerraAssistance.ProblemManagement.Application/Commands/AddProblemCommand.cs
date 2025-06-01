909771b956bb128275ddac29079ce4afbf2dca66
Nikolay Andreev <andreev_nikolay@outlook.com>
2025-06-01 08:41:55 +0400

    Add create problem functionality: implement ProblemCreate page and update ProblemList with a link to create new problems

--- TerraAssistance.ProblemManagement.Application/Commands/AddProblemCommand.cs
+++ TerraAssistance.ProblemManagement.Application/Commands/AddProblemCommand.cs
@@ -6,7 +6,7 @@ public class AddProblemCommand : IRequest
 {
     public AddProblemCommand(
         string title,
-        string description,
+        string? description,
         DateTime? estimatedAt)
     {
         Title = title;
