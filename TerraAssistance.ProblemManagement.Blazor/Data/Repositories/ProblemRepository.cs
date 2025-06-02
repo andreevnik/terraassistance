604ee58fc8fb91ca4cd5770c9d69d8d7faea1c31
Nikolay Andreev <andreev_nikolay@outlook.com>
2025-06-02 05:49:03 +0400

    Add comments functionality to ProblemDetails page and update ProblemRepository to include comments in queries

--- TerraAssistance.ProblemManagement.Blazor/Data/Repositories/ProblemRepository.cs
+++ TerraAssistance.ProblemManagement.Blazor/Data/Repositories/ProblemRepository.cs
@@ -1,5 +1,7 @@
 namespace TerraAssistance.ProblemManagement.Blazor.Data.Repositories;
 
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using TerraAssistance.ProblemManagement.Blazor.Data.Repositories.Abstract;
 using TerraAssistance.ProblemManagement.Domain.Entities;
 using TerraAssistance.ProblemManagement.Domain.Interfaces;
@@ -9,5 +11,10 @@ public class ProblemRepository : Repository<Problem>, IProblemRepository
     public ProblemRepository(ApplicationDbContext dbContext)
         : base(dbContext)
     {
+    }
+
+    protected override IQueryable<Problem> Query()
+    {
+        return DbSet.Include(problem => problem.Comments);
     }
 }
